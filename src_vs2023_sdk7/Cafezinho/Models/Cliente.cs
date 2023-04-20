e2c57f616794ec97d87a95b73757ca3a277bb41f
FlavioMartinsCruz <flaviomartinscruz@gmail.com>
2023-04-19 21:59:23 -0300

    Ajustes Model Clientes

--- src_vs2023_sdk7/Cafezinho/Models/Cliente.cs
+++ src_vs2023_sdk7/Cafezinho/Models/Cliente.cs
@@ -5,41 +5,51 @@ using System.ComponentModel.DataAnnotations;
 namespace Cafezinho.Models
 {
     [Table("clientes")]
-    [PrimaryKey(nameof(Cpf))]
+    // [PrimaryKey(nameof(Cpf))]
     public class Cliente
     {
         [Key]
-        [Required(ErrorMessage = "Campo Cpf é obrigatorio.")]
+        [Required(ErrorMessage = "Campo CPF é obrigatório.")]
         [Column(name: "cpf")]
+        [Display(Name ="CPF")]
         public string Cpf { get; set; }
-        [Required(ErrorMessage = "Campo Email é obrigatorio.")]
+
+        [Required(ErrorMessage = "Campo E-mail é obrigatório.")]
         [Column(name: "email")]
+        [Display(Name ="E-mail")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Campo Nome é obrigatorio.")]
+
+        [Required(ErrorMessage = "Campo Nome é obrigatório.")]
         [Column(name: "nome")]
         public string Nome { get; set; }
-        [Required(ErrorMessage = "Campo Cep é obrigatorio.")]
+
+        [Required(ErrorMessage = "Campo CEP é obrigatório.")]
         [Column(name: "cep")]
+        [Display(Name ="CEP")]
         public string Cep { get; set; }
-        [Required(ErrorMessage = "Campo Endereco é obrigatorio.")]
+
+        [Required(ErrorMessage = "Campo Endereço é obrigatório.")]
         [Column(name: "endereco")]
+        [Display(Name ="Endereço")]
         public string Endereco { get; set; }
-        [Required(ErrorMessage = "Campo Data de nascimento é obrigatorio.")]
+
+        [Required(ErrorMessage = "Campo Data de nascimento é obrigatório.")]
         [DataType(DataType.Date)]
         [Column(name: "dt_nascimento")]
+        [Display(Name ="Data de nascimento")]
         public DateTime DtNascimento { get; set; }
 
-        [Required(ErrorMessage = "Campo senha é obrigatorio.")]
+        [Required(ErrorMessage = "Campo Senha é obrigatório.")]
         [DataType(DataType.Password)]
         [Column(name: "senha")]
         public string Senha { get; set; }
 
         [Column(name: "carteira_id")]
-        
         [ForeignKey("CarteiraId")]
         public string? CarteiraId { get; set; }
 
-        [Required(ErrorMessage = "Campo perfil é obrigatorio.")]
+        [Required(ErrorMessage = "Campo Perfil de usuário é obrigatório.")]
+        [Display(Name ="Perfil de usuário")]
         public Perfil Perfil { get; set; }
     }
 
