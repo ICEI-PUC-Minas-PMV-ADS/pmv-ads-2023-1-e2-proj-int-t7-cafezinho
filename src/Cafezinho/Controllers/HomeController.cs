385356c585ece8df53a43a12216e69fa47583da6
Amanda de Almeida <economiaamanda@gmail.com>
2023-05-14 02:47:18 -0300

    popular home com registros

--- src/Cafezinho/Controllers/HomeController.cs
+++ src/Cafezinho/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using Cafezinho.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Cafezinho.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
-            _logger = logger;
+          
+            _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public async Task<IActionResult> Index()
+        {      
+
+         return View(await _context.Registros.ToListAsync());
+
         }
 
         public IActionResult Privacy()
