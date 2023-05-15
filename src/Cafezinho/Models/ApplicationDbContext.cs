5b3c6d8d86e8c7715af5e903dab811e839478185
Amanda de Almeida <economiaamanda@gmail.com>
2023-05-15 00:53:20 -0300

    Adicionar migrations para popular BD com admin

--- src/Cafezinho/Models/ApplicationDbContext.cs
+++ src/Cafezinho/Models/ApplicationDbContext.cs
@@ -9,12 +9,37 @@ namespace Cafezinho.Models
 {
     public class ApplicationDbContext : DbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
 
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Ativo> Ativos { get; set; }
         public DbSet<Registro> Registros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().HasData(
+                new Cliente()
+                {
+                    Cpf = "001",
+                    Nome = "Admin",
+                    Email = "",
+                    DtNascimento = DateTime.Now,
+                    Cep = "",
+                    Logradouro = "",
+                    Numero = "",
+                    Complemento = "",
+                    Bairro = "",
+                    Cidade = "",
+                    Estado = "",
+                    Senha = BCrypt.Net.BCrypt.HashPassword("1234"),
+                    Perfil = Perfil.Administrador,
+                }
+            );
+
+        }
     }
 }
