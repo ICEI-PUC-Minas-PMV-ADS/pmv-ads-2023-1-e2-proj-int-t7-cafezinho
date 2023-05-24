a161c94ef6ad8cb023cda3c418bfa4fef2fec191
Diego <ruas.diegotoledo@gmail.com>
2023-05-23 21:13:03 -0300

    DashboardsWIP

---
+++ src/Cafezinho/Models/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cafezinho.Models
+{
+    public class Dashboard
+    {
+        [Key]
+        public int DashboardId { get; set; }
+
+        private DateTime dataInicio;
+        private DateTime dataFim;
+        private int numeroDias;
+
+        public int TotalEmCarteira { get; set; }
+        public List<Ativo> ativos { get; set; }
+        public List<Registro> Registros { get; set; }
+    }
+}
